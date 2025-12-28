2d9cf83ea189401657f1d58ebe330463cbd97b60
ktsuchimoto524-collab <k.tsuchimoto524@gmail.com>
2025-12-28 15:50:21 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import cv2
 import random
 import urllib.parse
 
+st.markdown( '<meta name="google-site-verification" content="UOB8ksPEWmyI2-x2bCPRcwEWxcQKJPbmeSt6mA_EjX4" />', unsafe_allow_html=True )
+
 # ======================
 # 画像処理本体
 # ======================
